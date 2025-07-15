a3a41f6102ac5417f7d35552ebcda82fee5842c2
Larry Jones <mrwizard82d1@gmail.com>
2025-07-15 18:07:39 -0500

    Use `cond` to handle multiple conditions

--- act-ch-01/c02_estimate.clj
+++ act-ch-01/c02_estimate.clj
@@ -18,9 +18,7 @@
   "Enter a state of meditation depending on pre-existing calmness level"
   [s calmness-level]
   (println "Clojure Meditate 2.0")
-  (if (< calmness-level 5)
-    (str (clojure.string/upper-case s) ", I TELL YA!")
-    (if (<= 5 calmness-level 9)
-      (str (clojure.string/capitalize s) "!")
-      (if (= calmness-level 10)
-        (clojure.string/reverse s)))))
+  (cond
+    (< calmness-level 5) (str (clojure.string/upper-case s) ", I TELL YA!")
+    (<= 5 calmness-level 9) (str (clojure.string/capitalize s) "!")
+    (= calmness-level 10) (clojure.string/reverse s)))
