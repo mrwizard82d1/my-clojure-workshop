ab70b18450ecf1b2f7ad68ec3f0472d800320f47
Larry Jones <mrwizard82d1@gmail.com>
2025-08-12 00:22:11 -0500

    Skeleton project to convert formats

---
+++ ex08/format-conversion/test/format_conversion/core_test.clj
@@ -0,0 +1,7 @@
+(ns format-conversion.core-test
+  (:require [clojure.test :refer :all]
+            [format-conversion.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
