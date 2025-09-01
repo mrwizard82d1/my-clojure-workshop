b015b88abf8a6c076421dbc099c7f7a02ef85200
Larry Jones <mrwizard82d1@gmail.com>
2025-08-31 23:55:56 -0500

    Chapter 01 activity 01

---
+++ ch-01/src/ch_01/activity_01.clj
@@ -0,0 +1,13 @@
+(ns ch-01.activity-01)
+
+;; Code for Chapter 01, Activity 01
+;;
+
+;; Print an encouraging message
+(println "I am not afraid of parentheses.")
+
+;; Complicated arithmetic
+(* (+ 1 2 3) (- 10 3))
+
+;; Print a congratulatory message
+(println "Well done, my young Padawan")
