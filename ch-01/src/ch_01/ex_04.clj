5198e698f3f76884d47155cc4a6534b1cd2b8faa
Larry Jones <mrwizard82d1@gmail.com>
2025-09-01 14:46:30 -0500

    Using `def` and `let` to create bindings

---
+++ ch-01/src/ch_01/ex_04.clj
@@ -0,0 +1,65 @@
+(ns ch-01.ex-04)
+
+;; Bindings
+;;
+;; - `def` for global bindings
+;; - `let` for local bindings
+;;
+
+;; Use `def` to bind a symbol to a value. For example, the following form
+;; binds the value, 10, to the symbol, `x` in **this namespace**.
+(def x 10)
+x
+
+;; Technically, one can change a binding created with `def`; however,
+;; is is only "fine" when experimenting in the REPL.
+(def x 20)
+x
+
+;; This technique is **not** recommended for programs:
+;;
+;; - It can make code harder to read
+;; - It con complicate maintenance
+;; - It is so very **unexpected**.
+
+;; After we have bound `x` to a value, we can use it in expressions
+(inc x)
+
+;; Again, this usage **does not** change the value of `x`
+x
+
+;; Remember that `def` only binds symbols and values
+;; **in the current namespace**. A `def` binding for a symbol in this
+;; namespace **does not** mean that binding is available in another
+;; namespace.
+
+;; Don't use the following expression in your code.
+x
+(do (def x 42))
+x
+
+;; Remember, any use of `def` affects the **global** scope of the source
+;; file no matter how **deeply** the `def` is encountered in other
+;; expressions.
+
+;; Additionally, remember that a symbol bound to a value using `def` is
+;; **automatically namespaced**. This default action helps avoid clashes
+;; between existing name is **different** namespaces.
+
+;; The special form, `let`, allows us to create **new bindings** within
+;; a **local** scope (the scope of the `let` form).
+(let [y 3]
+  (println y)
+  (* 10 y))
+
+;; If one now tries to access the value of `y`, an exception will be raised
+#_(try
+  (println y)
+  (catch Exception e
+    (println (.getMessage e))))
+
+;; But how do `let` bindings affect `def` bindings? As the crew head says
+;; in "The Hitchhikers Guide to the Galaxy", "Not at all."
+(let [x 3]
+  (println x))
+x
