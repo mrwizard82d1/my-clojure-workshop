9b289ada8a6aa59adc4271838e0f5169757202ee
Larry Jones <mrwizard82d1@gmail.com>
2026-02-09 11:31:22 -0600

    Display image on home page

--- ch-15/packt-clj.reagent-sandbox/src/packt_clj/reagent_sandbox/core.cljs
+++ ch-15/packt-clj.reagent-sandbox/src/packt_clj/reagent_sandbox/core.cljs
@@ -1,5 +1,5 @@
 (ns packt-clj.reagent-sandbox.core
-    (:require 
+    (:require
               [reagent.core :as reagent :refer [atom]]
               [reagent.dom :as rd]))
 
@@ -12,16 +12,22 @@
 (defonce app-state (atom {:text "Hello world!"}))
 
 
+(defn image [url]
+  [:img {:src url
+         :style {:width "500px"
+                 :border "solid gray 3px"
+                 :border-radius "10px"}}])
+
 (defn hello-world []
   [:div
    [:h1 (:text @app-state)]
+   [:div [image "https://picsum.photos/id/0/5000/3333"]]
    [:h3 "Edit this and watch it change!"]])
 
 (rd/render [hello-world]
            (. js/document (getElementById "app")))
 
-(defn on-js-reload []
+(defn on-js-reload [])
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
